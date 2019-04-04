1ebdb1a4cc36eec23526c6a8cd9796c94d10ac28
msokalski <msokalski@atsisa.com>
2019-04-04 17:29:00 +0200

    .

---
+++ fast_rand.h
@@ -0,0 +1,16 @@
+#pragma once
+
+static unsigned int g_seed = 0x87654321;
+
+// Used to seed the generator.           
+inline void fast_srand(int seed) {
+	g_seed = seed;
+}
+
+// Compute a pseudorandom integer.
+// Output value in range [0, 32767]
+#define FAST_RAND_MAX 0x7fff
+inline int fast_rand(void) {
+	g_seed = (214013 * g_seed + 2531011);
+	return (g_seed >> 16) & 0x7FFF;
+}
