becf6f72fc5e772ecb711a35a2fcb62e96e465e7
msokalski <m.sokalski@3dweb.pl>
2020-03-15 19:30:28 +0100

    .

---
+++ io_mesh_akm/__init__.py
@@ -0,0 +1,260 @@
+
+bl_info = {
+    "name": "Asciicker AKM format",
+    "author": "Gumix",
+    "version": (1, 1, 0),
+    "blender": (2, 82, 0),
+    "location": "File > Import-Export",
+    "description": "Import-Export AKM mesh data",
+    "wiki_url": "https://asciicker.com/",
+    "support": 'OFFICIAL',
+    "category": "Import-Export",
+}
+
+# Copyright (C) 2020: Gumix
+
+if "bpy" in locals():
+    import importlib
+    if "export_akm" in locals():
+        importlib.reload(export_akm)
+    if "import_akm" in locals():
+        importlib.reload(import_akm)
+
+
+import bpy
+from bpy.props import (
+    CollectionProperty,
+    StringProperty,
+    BoolProperty,
+    FloatProperty,
+)
+from bpy_extras.io_utils import (
+    ImportHelper,
+    ExportHelper,
+    axis_conversion,
+    orientation_helper
+)
+
+
+class ImportAKM(bpy.types.Operator, ImportHelper):
+    """Load a AKM geometry file"""
+    bl_idname = "import_mesh.akm"
+    bl_label = "Import AKM"
+    bl_options = {'UNDO'}
+
+    files: CollectionProperty(
+        name="File Path",
+        description=(
+            "File path used for importing "
+            "the AKM file"
+        ),
+        type=bpy.types.OperatorFileListElement)
+
+    directory: StringProperty()
+
+    filename_ext = ".akm"
+    filter_glob: StringProperty(default="*.akm", options={'HIDDEN'})
+
+    def execute(self, context):
+        import os
+
+        paths = [os.path.join(self.directory, name.name)
+                 for name in self.files]
+        if not paths:
+            paths.append(self.filepath)
+
+        from . import import_akm
+
+        for path in paths:
+            import_akm.load(self, context, path)
+
+        return {'FINISHED'}
+
+
+@orientation_helper(axis_forward='Y', axis_up='Z')
+class ExportAKM(bpy.types.Operator, ExportHelper):
+    bl_idname = "export_mesh.akm"
+    bl_label = "Export AKM"
+    bl_description = "Export as Asciicker mesh"
+
+    filename_ext = ".akm"
+    filter_glob: StringProperty(default="*.akm", options={'HIDDEN'})
+
+    use_selection: BoolProperty(
+        name="Selection Only",
+        description="Export selected objects only",
+        default=False,
+    )
+    use_mesh_modifiers: BoolProperty(
+        name="Apply Modifiers",
+        description="Apply Modifiers to the exported mesh",
+        default=True,
+    )
+    use_normals: BoolProperty(
+        name="Normals",
+        description=(
+            "Export Normals for smooth and "
+            "hard shaded faces "
+            "(hard shaded faces will be exported "
+            "as individual faces)"
+        ),
+        default=True,
+    )
+    use_uv_coords: BoolProperty(
+        name="UVs",
+        description="Export the active UV layer",
+        default=True,
+    )
+    use_colors: BoolProperty(
+        name="Vertex Colors",
+        description="Export the active vertex color layer",
+        default=True,
+    )
+
+    global_scale: FloatProperty(
+        name="Scale",
+        min=0.01, max=1000.0,
+        default=1.0,
+    )
+
+    def execute(self, context):
+        from . import export_akm
+
+        from mathutils import Matrix
+
+        keywords = self.as_keywords(
+            ignore=(
+                "axis_forward",
+                "axis_up",
+                "global_scale",
+                "check_existing",
+                "filter_glob",
+            )
+        )
+        global_matrix = axis_conversion(
+            to_forward=self.axis_forward,
+            to_up=self.axis_up,
+        ).to_4x4() @ Matrix.Scale(self.global_scale, 4)
+        keywords["global_matrix"] = global_matrix
+
+        filepath = self.filepath
+        filepath = bpy.path.ensure_ext(filepath, self.filename_ext)
+
+        return export_akm.save(self, context, **keywords)
+
+    def draw(self, context):
+        pass
+
+
+class AKM_PT_export_include(bpy.types.Panel):
+    bl_space_type = 'FILE_BROWSER'
+    bl_region_type = 'TOOL_PROPS'
+    bl_label = "Include"
+    bl_parent_id = "FILE_PT_operator"
+
+    @classmethod
+    def poll(cls, context):
+        sfile = context.space_data
+        operator = sfile.active_operator
+
+        return operator.bl_idname == "EXPORT_MESH_OT_akm"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False  # No animation.
+
+        sfile = context.space_data
+        operator = sfile.active_operator
+
+        layout.prop(operator, "use_selection")
+
+
+class AKM_PT_export_transform(bpy.types.Panel):
+    bl_space_type = 'FILE_BROWSER'
+    bl_region_type = 'TOOL_PROPS'
+    bl_label = "Transform"
+    bl_parent_id = "FILE_PT_operator"
+
+    @classmethod
+    def poll(cls, context):
+        sfile = context.space_data
+        operator = sfile.active_operator
+
+        return operator.bl_idname == "EXPORT_MESH_OT_akm"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False  # No animation.
+
+        sfile = context.space_data
+        operator = sfile.active_operator
+
+        layout.prop(operator, "axis_forward")
+        layout.prop(operator, "axis_up")
+        layout.prop(operator, "global_scale")
+
+
+class AKM_PT_export_geometry(bpy.types.Panel):
+    bl_space_type = 'FILE_BROWSER'
+    bl_region_type = 'TOOL_PROPS'
+    bl_label = "Geometry"
+    bl_parent_id = "FILE_PT_operator"
+
+    @classmethod
+    def poll(cls, context):
+        sfile = context.space_data
+        operator = sfile.active_operator
+
+        return operator.bl_idname == "EXPORT_MESH_OT_akm"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False  # No animation.
+
+        sfile = context.space_data
+        operator = sfile.active_operator
+
+        layout.prop(operator, "use_mesh_modifiers")
+        layout.prop(operator, "use_normals")
+        layout.prop(operator, "use_uv_coords")
+        layout.prop(operator, "use_colors")
+
+
+def menu_func_import(self, context):
+    self.layout.operator(ImportAKM.bl_idname, text="Asciicker (.akm)")
+
+
+def menu_func_export(self, context):
+    self.layout.operator(ExportAKM.bl_idname, text="Asciicker (.akm)")
+
+
+classes = (
+    ImportAKM,
+    ExportAKM,
+    AKM_PT_export_include,
+    AKM_PT_export_transform,
+    AKM_PT_export_geometry,
+)
+
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+
+
+def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+
+
+if __name__ == "__main__":
+    register()
---
+++ io_mesh_akm/export_akm.py
@@ -0,0 +1,338 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# <pep8-80 compliant>
+
+"""
+This script exports Stanford PLY files from Blender. It supports normals,
+colors, and texture coordinates per face or per vertex.
+"""
+
+
+
+"""
+# SHAPE KEYS AS MORPH TARGETS
+import bpy
+
+obj1 = bpy.context.selected_objects[0]
+
+for key in obj1.data.shape_keys.key_blocks:
+    print("NAME:", key.name)
+    print("INTERPOLATION:", key.interpolation)
+    print("MUTE:", key.mute)
+    print("RELATIVE_TO:", key.relative_key.name)
+    print("SLIDER_MAX:", key.slider_max)
+    print("SLIDER_MIN:", key.slider_min)
+    print("SLIDER_VAL:", key.value)
+    print("VERTEX_GROUP:", key.vertex_group)
+....
+    for v in key.data:
+        print(v.co)
+
+#for vert in obj1.data.vertices:
+#    print(vert.co)  # this is a vertex coord of the mesh
+"""
+
+
+
+"""
+# ISOLATED EDGES AS LINES !!!
+import bpy
+import bmesh
+
+obj1 = bpy.context.selected_objects[0]
+
+bm = bmesh.new()
+bm.from_mesh(obj1.data)
+
+bmesh.ops.triangulate(bm, faces=bm.faces)
+
+
+print("---------------------------------")
+
+edges = []
+
+for e in bm.edges:
+    edges.append( { e.verts[0].index, e.verts[1].index } )
+    
+for f in bm.faces:
+    for r in f.edges:
+        
+        r1 = { r.verts[0].index, r.verts[1].index }
+        r2 = { r.verts[1].index, r.verts[0].index }
+        
+        try:
+            edges.remove(r1);
+        except:
+            pass
+
+        try:
+            edges.remove(r2);
+        except:
+            pass
+                                
+for e in edges:
+    print( e )
+        
+"""
+
+
+
+
+
+
+
+def save_mesh(filepath, mesh, obj, use_normals=True, use_uv_coords=True, use_colors=True):
+    import os
+    import bpy
+
+    def rvec3d(v):
+        return round(v[0], 6), round(v[1], 6), round(v[2], 6)
+
+    def rvec2d(v):
+        return round(v[0], 6), round(v[1], 6)
+
+    # NO LUCK! Be sure tessellated loop trianlges are available!
+    #if not mesh.loop_triangles and mesh.polygons:
+    #    mesh.calc_loop_triangles()
+
+    if use_uv_coords and mesh.uv_layers:
+        active_uv_layer = mesh.uv_layers.active.data
+    else:
+        use_uv_coords = False
+
+    if use_colors and mesh.vertex_colors:
+        active_col_layer = mesh.vertex_colors.active.data
+    else:
+        use_colors = False
+
+    # in case
+    color = uvcoord = uvcoord_key = normal = normal_key = None
+
+    mesh_verts = mesh.vertices
+    # vdict = {} # (index, normal, uv) -> new index
+    vdict = [{} for i in range(len(mesh_verts))]
+    ply_verts = []
+    ply_faces = [[] for f in range(len(mesh.polygons))]
+    vert_count = 0
+
+    for i, f in enumerate(mesh.polygons):
+
+        smooth = not use_normals or f.use_smooth
+        if not smooth:
+            normal = f.normal[:]
+            normal_key = rvec3d(normal)
+
+        if use_uv_coords:
+            uv = [
+                active_uv_layer[l].uv[:]
+                for l in range(f.loop_start, f.loop_start + f.loop_total)
+            ]
+        if use_colors:
+            col = [
+                active_col_layer[l].color[:]
+                for l in range(f.loop_start, f.loop_start + f.loop_total)
+            ]
+
+        pf = ply_faces[i]
+        for j, vidx in enumerate(f.vertices):
+            v = mesh_verts[vidx]
+
+            if smooth:
+                normal = v.normal[:]
+                normal_key = rvec3d(normal)
+
+            if use_uv_coords:
+                uvcoord = uv[j][0], uv[j][1]
+                uvcoord_key = rvec2d(uvcoord)
+
+            if use_colors:
+
+                weight = 0.0
+                for g in v.groups:
+                    if g.group == obj.vertex_groups.active.index:
+                        weight = obj.vertex_groups.active.weight(vidx)
+                        break
+
+                color = col[j]
+                color = (
+                    int(round(color[0] * 255.0)),
+                    int(round(color[1] * 255.0)),
+                    int(round(color[2] * 255.0)),
+                    int(round(weight * 255.0)),
+                )
+
+                #color = col[j]
+                #color = (
+                #    int(color[0] * 255.0),
+                #    int(color[1] * 255.0),
+                #    int(color[2] * 255.0),
+                #    int(color[3] * 255.0),
+                #)
+
+            key = normal_key, uvcoord_key, color
+
+            vdict_local = vdict[vidx]
+            pf_vidx = vdict_local.get(key)  # Will be None initially
+
+            if pf_vidx is None:  # Same as vdict_local.has_key(key)
+                pf_vidx = vdict_local[key] = vert_count
+                ply_verts.append((vidx, normal, uvcoord, color))
+                vert_count += 1
+
+            pf.append(pf_vidx)
+
+    with open(filepath, "w", encoding="utf-8", newline="\n") as file:
+        fw = file.write
+
+        # Header
+        # ---------------------------
+
+        fw("ply\n")
+        fw("format ascii 1.0\n")
+        fw(
+            f"comment Created by Blender {bpy.app.version_string} - "
+            f"www.blender.org, source file: {os.path.basename(bpy.data.filepath)!r}\n"
+        )
+
+        fw(f"element vertex {len(ply_verts)}\n")
+        fw(
+            "property float x\n"
+            "property float y\n"
+            "property float z\n"
+        )
+        if use_normals:
+            fw(
+                "property float nx\n"
+                "property float ny\n"
+                "property float nz\n"
+            )
+        if use_uv_coords:
+            fw(
+                "property float s\n"
+                "property float t\n"
+            )
+        if use_colors:
+            fw(
+                "property uchar red\n"
+                "property uchar green\n"
+                "property uchar blue\n"
+                "property uchar alpha\n"
+            )
+
+        fw(f"element face {len(mesh.polygons)}\n")
+        fw("property list uchar uint vertex_indices\n")
+
+        fw("end_header\n")
+
+        # Vertex data
+        # ---------------------------
+
+        for i, v in enumerate(ply_verts):
+            fw("%.6f %.6f %.6f" % mesh_verts[v[0]].co[:])
+            if use_normals:
+                fw(" %.6f %.6f %.6f" % v[1])
+            if use_uv_coords:
+                fw(" %.6f %.6f" % v[2])
+            if use_colors:
+                fw(" %u %u %u %u" % v[3])
+            fw("\n")
+
+        # Face data
+        # ---------------------------
+
+        for pf in ply_faces:
+            fw(f"{len(pf)}")
+            for v in pf:
+                fw(f" {v}")
+            fw("\n")
+
+        print(f"Writing {filepath!r} done")
+
+    return {'FINISHED'}
+
+
+def save(
+    operator,
+    context,
+    filepath="",
+    use_selection=False,
+    use_mesh_modifiers=True,
+    use_normals=True,
+    use_uv_coords=True,
+    use_colors=True,
+    global_matrix=None
+):
+    import bpy
+    import bmesh
+
+    if bpy.ops.object.mode_set.poll():
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+    if use_selection:
+        obs = context.selected_objects
+    else:
+        obs = context.scene.objects
+
+    depsgraph = context.evaluated_depsgraph_get()
+    bm = bmesh.new()
+
+    for ob in obs:
+        if use_mesh_modifiers:
+            ob_eval = ob.evaluated_get(depsgraph)
+        else:
+            ob_eval = ob
+
+        try:
+            me = ob_eval.to_mesh()
+        except RuntimeError:
+            continue
+
+        me.transform(ob.matrix_world)
+        bm.from_mesh(me)
+        ob_eval.to_mesh_clear()
+
+    mesh = bpy.data.meshes.new("TMP PLY EXPORT")
+
+    #pray!
+    bmesh.ops.triangulate(bm, faces=bm.faces)
+    #, quad_method=0, ngon_method=0)
+
+    bm.to_mesh(mesh)
+    bm.free()
+
+    if global_matrix is not None:
+        mesh.transform(global_matrix)
+
+    if use_normals:
+        mesh.calc_normals()
+
+    obj = context.active_object
+
+    ret = save_mesh(
+        filepath,
+        mesh,
+        obj,
+        use_normals=use_normals,
+        use_uv_coords=use_uv_coords,
+        use_colors=use_colors,
+    )
+
+    bpy.data.meshes.remove(mesh)
+
+    return ret
