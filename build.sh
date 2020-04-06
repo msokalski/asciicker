92d74139799424d0e92143390dbfc5d2472bb472
msokalski <>
2020-04-06 07:55:46 -0400

    .

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-/usr/bin/time -f "----------------------\ndone in %e sec" make -j16 -f makefile_asciiid
-/usr/bin/time -f "----------------------\ndone in %e sec" make -j16 -f makefile_server
-/usr/bin/time -f "----------------------\ndone in %e sec" make -j16 -f makefile_game
+if [ -f "/usr/bin/time" ]; then
+    /usr/bin/time -f "----------------------\ndone in %e sec" make -j16 -f makefile_asciiid
+    /usr/bin/time -f "----------------------\ndone in %e sec" make -j16 -f makefile_server
+    /usr/bin/time -f "----------------------\ndone in %e sec" make -j16 -f makefile_game
+else
+    make -j16 -f makefile_asciiid
+    make -j16 -f makefile_server
+    make -j16 -f makefile_game
+fi
 
