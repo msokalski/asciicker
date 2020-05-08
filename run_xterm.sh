05f56c9b94d25326f31ef5016e50877f75189288
gumix <gumix@3dweb.pl>
2020-05-08 21:45:23 +0200

    .

--- run_xterm.sh
+++ run_xterm.sh
@@ -4,11 +4,12 @@ xset fp+ /home/user/asciiid/fonts
 xset fp rehash
 
 #run
-xterm -e .run/game_term \
+xterm \
     -xrm "xterm*font1: -gumix-*-*-*-*-*-8-*-*-*-*-*-*" \
     -xrm "xterm*font2: -gumix-*-*-*-*-*-10-*-*-*-*-*-*" \
     -xrm "xterm*font3: -gumix-*-*-*-*-*-12-*-*-*-*-*-*" \
     -xrm "xterm*font:  -gumix-*-*-*-*-*-14-*-*-*-*-*-*" \
     -xrm "xterm*font4: -gumix-*-*-*-*-*-16-*-*-*-*-*-*" \
     -xrm "xterm*font5: -gumix-*-*-*-*-*-18-*-*-*-*-*-*" \
-    -xrm "xterm*font6: -gumix-*-*-*-*-*-20-*-*-*-*-*-*"
+    -xrm "xterm*font6: -gumix-*-*-*-*-*-20-*-*-*-*-*-*" \
+    -e .run/game_term 
