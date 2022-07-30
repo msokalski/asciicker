22607587d6238096fb6ce5d3bc5d97f687490605
Niki4tap <rombiklol2@gmail.com>
2022-07-31 02:30:35 +0300

    Revert clangd complains again

--- src/x11.cpp
+++ src/x11.cpp
@@ -1964,7 +1964,7 @@ int a3dListDir(const char* dir_path, bool (*cb)(A3D_DirItem item, const char* na
 			if (s.st_mode == S_IFDIR)
 				item = A3D_DIRECTORY;
 			else
-			if (s.st_mode == S_IFREG)
+			if (dir->d_type == S_IFREG)
 				item = A3D_FILE;
 			else
 				continue;				
